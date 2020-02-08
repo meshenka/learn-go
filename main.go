f366287cac60068ea9fba919023cf459f4a45aa4
Sylvain Gogel <sylvain.gogel@gmail.com>
2020-02-08 15:00:24 +0100

    move NewServer to it's own package for reusability

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"time"
+
+	server "./server"
 )
 
 const message = "Hello Wold!"
@@ -31,48 +31,11 @@ func main() {
 		w.Write([]byte(message))
 	})
 
-	srv := NewServer(mux, MlgServiceAddr)
+	srv := server.New(mux, MlgServiceAddr)
 
 	err := srv.ListenAndServeTLS(MlgCertFile, MlgKeyFile)
 
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
-}
-
-// NewServer create the server
-func NewServer(mux *http.ServeMux, serverAddress string) *http.Server {
-	//https://blog.cloudflare.com/exposing-go-on-the-internet/
-	tlsConfig := &tls.Config{
-		PreferServerCipherSuites: true,
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.X25519,
-		},
-		MinVersion: tls.VersionTLS12,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-
-			// Best disabled, as they don't provide Forward Secrecy,
-			// but might be necessary for some clients
-			// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
-		},
-	}
-
-	srv := &http.Server{
-		Addr:         serverAddress,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-		TLSConfig:    tlsConfig,
-		Handler:      mux,
-	}
-
-	return srv
 }
