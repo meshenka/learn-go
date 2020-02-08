b1caac470cade35531f7ff7ae0e0d4c7a9a79bbe
Sylvain Gogel <sylvain.gogel@gmail.com>
2020-02-08 12:59:08 +0100

    add headers

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"net/http"
 )
+
 const message = "Hello Wold!"
 
-func main()  {
+func main() {
 	fmt.Println(message)
-	
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(message))
 	})
+
 	http.ListenAndServe(":8080", mux)
 }
